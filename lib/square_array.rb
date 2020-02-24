c16cc7c7f6adabe287f132f292d6b174b344835d
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-02-24 23:31:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   counter = 0 
   numbers =[1,2,3]
   
-  while numbers[counter]  do
+  while numbers[counter] < numbers.length do
    puts numbers[counter] ** 2
     counter +=1
   end
